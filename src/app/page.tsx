497d5e199494e30185678328a86ca79889c2f0c8
micahwierenga <wierenga.micah@gmail.com>
2025-04-26 22:07:16 -0600

    refactor: extracts fetch request into function, includes search term in request, and cleans up code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,41 +15,25 @@ interface Advocate {
 }
 
 export default function Home() {
-  const [advocates, setAdvocates] = useState([]);
   const [filteredAdvocates, setFilteredAdvocates] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    console.log("fetching advocates...");
-    fetch("/api/advocates").then((response) => {
-      response.json().then((jsonResponse) => {
-        setAdvocates(jsonResponse.data);
-        setFilteredAdvocates(jsonResponse.data);
-      });
-    });
+    fetchAdvocates('');
   }, []);
 
+  const fetchAdvocates = async (searchTerm: string) => {
+    const response = await fetch(`/api/advocates?search=${searchTerm}`);
+    const jsonResponse = await response.json();
+
+    setFilteredAdvocates(jsonResponse.data);
+  }
+
   const onChange: ChangeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(e.target.value);
-
-    console.log("filtering advocates...");
-    const filteredAdvocates = advocates.filter((advocate: Advocate) => {
-      return (
-        advocate.firstName.includes(searchTerm) ||
-        advocate.lastName.includes(searchTerm) ||
-        advocate.city.includes(searchTerm) ||
-        advocate.degree.includes(searchTerm) ||
-        advocate.specialties.includes(searchTerm) ||
-        advocate.yearsOfExperience == parseInt(searchTerm)
-      );
-    });
-
-    setFilteredAdvocates(filteredAdvocates);
+    fetchAdvocates(e.target.value);
   };
 
   const onClick = () => {
-    console.log(advocates);
-    setFilteredAdvocates(advocates);
+    fetchAdvocates('');
   };
 
   return (
